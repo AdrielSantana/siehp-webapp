26156450140eb08622457767d10a74b9444c67cf
Adriel Santana <diel_san@hotmail.com>
2024-03-02 02:53:39 -0300

    feat(shared): Add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,107 @@
 /** @type {import('tailwindcss').Config} */
 
+import { createThemes } from "tw-colors";
+
 module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/shared/infra/factories/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/shared/infra/factories/layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/shared/infra/presentation/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/shared/infra/presentation/layouts/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    spacing: {
+      "0.5": "0.5rem",
+      "0.75": "0.75rem",
+      "1": "1rem",
+      "1.5": "1.5rem",
+      "2": "2rem",
+      "2.5": "2.5rem",
+      "3": "3rem",
+      "3.5": "3.5rem",
+      "4": "4rem",
+      "4.5": "4.5rem",
+      "5": "5rem",
+      "6": "6rem",
+      "7": "7rem",
+      "8": "8rem",
+      "9": "9rem",
+      "10": "10rem",
+    },
+    colors: {
+      gray: {
+        "100": "#F9F9F9",
+        "300": "#D1D1D1",
+        "500": "#717171",
+        "700": "#525252",
+        "900": "#212121",
+      },
+    },
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "425px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
-    
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+    fontSize: {
+      sm: "0.75rem",
+      md: "1rem",
+      lg: "1.5rem",
+      xl: "2rem",
     },
-    extend: {
-      colors: {
-        blue: '#1fb6ff',
-        purple: '#7e5bef',
-        pink: '#ff49db',
-        orange: '#ff7849',
-        green: '#13ce66',
-        yellow: '#ffc82c',
-        gray_dark: '#273444',
-        gray: '#8492a6',
-        gray_light: '#d3dce6',
-        header: '#474747',
-        footer: '#474747',
-      },
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-      backgroundColor: {
-        'light-mode': '#F9F9F9',
-        'dark-mode': '#212121',
-      },
+    fontWeight: {
+      light: "300",
+      regular: "400",
+      "semi-bold": "500",
+      bold: "700",
     },
+    extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    createThemes(
+      {
+        light: {
+          gray: {
+            "100-tk": "#F9F9F9",
+            "300-tk": "#D1D1D1",
+            "500-tk": "#717171",
+            "700-tk": "#525252",
+            "900-tk": "#212121",
+          },
+        },
+        dark: {
+          gray: {
+            "100-tk": "#212121",
+            "300-tk": "#525252",
+            "500-tk": "#717171",
+            "700-tk": "#D1D1D1",
+            "900-tk": "#F9F9F9",
+          },
+        },
+        normal: {
+          sucess: "#04FF00",
+          warning: "#EBFF00",
+          fail: "#FF0000",
+        },
+        protanopia: {
+          sucess: "#F8DB00",
+          warning: "#FFF2C3",
+          fail: "#8F7E1E",
+        },
+        deuteranopia: {
+          sucess: "#FFD499",
+          warning: "#FFEFDF",
+          fail: "#A17700",
+        },
+        tritanopia: {
+          sucess: "#75ECFF",
+          warning: "#FFECFE",
+          fail: "#FD1700",
+        },
+      },
+      {
+        produceThemeClass: (themeName) => `theme-${themeName}`,
+        strict: true,
+        defaultTheme: {
+          light: "light",
+          dark: "dark",
+        },
+      },
+    ),
+  ],
+};
