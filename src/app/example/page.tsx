5b0d6d597603f5333987db82817176f814c94982
Adriel Santana <diel_san@hotmail.com>
2024-03-02 03:28:18 -0300

    feat(shared): metadata

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -2,5 +2,5 @@ import MakeExamplePage from "@/shared/infra/factories/pages/MakeExamplePage";
 
 export default function ExamplePage() {
   // return <MakeExamplePage />;
-  return <></>
+  return <></>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import MakeLandingPageLayout from "@/shared/infra/factories/layouts/MakeLandingPageLayout";
 import MakeWorkInProgressPage from "@/shared/infra/factories/pages/MakeWorkInProgressPage";
 
+export const metadata = { title: "SIEHP | Em Construção" };
+
 export default function Home() {
   return (
     <MakeLandingPageLayout>
