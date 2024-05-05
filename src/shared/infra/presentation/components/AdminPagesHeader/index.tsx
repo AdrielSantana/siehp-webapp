d4b4d5784960f048a8959daa54fecaf372ec6b74
athena272 <guilhermera272@gmail.com>
2024-05-05 08:10:02 -0300

    stylized search icon

--- src/shared/infra/presentation/components/AdminPagesHeader/index.tsx
+++ src/shared/infra/presentation/components/AdminPagesHeader/index.tsx
@@ -32,48 +32,45 @@ const AdminPagesHeaderTitle = ({
   );
 };
 
-const AdminPagesHeaderBody=({children, className, ...props}:AdminPagesHeaderBodyProps)=>{
-  return (<div className={`flex flex-grow justify-end items-center ${className?? ""}`} {...props}>
+const AdminPagesHeaderBody = ({ children, className, ...props }: AdminPagesHeaderBodyProps) => {
+  return (<div className={`flex flex-grow justify-end items-center ${className ?? ""}`} {...props}>
     {children}
   </div>);
 }
 
-const AdminPagesHeaderButtonContent = ({children, className, ...props}: AdminPagesHeaderButtonContentProps)=>{
+const AdminPagesHeaderButtonContent = ({ children, className, ...props }: AdminPagesHeaderButtonContentProps) => {
   return (
-    <div className={`flex flex-wrap ${className??""}`} {...props}>
+    <div className={`flex flex-wrap ${className ?? ""}`} {...props}>
       {children}
     </div>
   )
 }
 
-const AdminPagesHeaderButton = ({children, className, ...props}: AdminPagesHeaderButtonProps)=>{
+const AdminPagesHeaderButton = ({ children, className, ...props }: AdminPagesHeaderButtonProps) => {
   return (
     <button className={`mr-4 ${className ?? ""}`} {...props}>{children}</button>
   );
 }
 
-const AdminPagesHeaderButtonIcon = ({children, className, ...props}: AdminPagesHeaderButtonIconProps)=>{
+const AdminPagesHeaderButtonIcon = ({ children, className, ...props }: AdminPagesHeaderButtonIconProps) => {
   return (<div className={`w-1 h-[1.125rem] mr-1  ${className ?? ""}`} {...props}>
     {children}
   </div>)
 }
 
-const AdminPagesHeaderFilter = ({className, ...props}: AdminPagesHeaderFilterProps)=>{
-  return(
-    <div>
+const AdminPagesHeaderFilter = ({ className, ...props }: AdminPagesHeaderFilterProps) => {
+  return (
+    <div className="relative">
       <input
         type="text"
         placeholder="Pesquisar..."
-    {...props}
-    className={`rounded-[4rem] h-2.5 w-[15rem] bg-gray-700-tk px-1 py-[0.7rem] appearance-none border-none text-gray-100-tk text-1 placeholder-gray-100-tk placeholder-1  ${className ?? ""}`}
-  >
-  
-
-  </input>
-  <SearchIcon className="[&>path]:fill-gray-300-tk" width={24} height={24} viewBox="0 0 30 24" />
+        {...props}
+        className={`rounded-[4rem] h-2.5 w-[15rem] bg-gray-700-tk pl-1 pr-[45px] py-[0.7rem] appearance-none border-none text-gray-100-tk text-1 placeholder-gray-100-tk placeholder-1  ${className ?? ""}`}
+      />
+      <SearchIcon className="[&>path]:fill-gray-300-tk absolute top-[6px] right-[15px]" width={24} height={24} viewBox="0 0 30 24" />
     </div>
 
-)
+  )
 }
 
 export const AdminPagesHeader = {
